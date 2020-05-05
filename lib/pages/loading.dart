9bfc0ecd832db21e4013de05864afc00f18350da
kimiya zargari <kimiyazargari13@gmail.com>
2020-05-05 11:15:45 +0430

    rewrite with dio

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:gson/gson.dart';
-import 'package:http/http.dart';
+
 import 'package:loginapp/weather.dart';
+import 'package:dio/dio.dart';
 
 class Loading extends StatefulWidget {
   Map loginData = {};
@@ -18,14 +18,23 @@ class _LoadingState extends State<Loading> {
     getWeather();
   }
   void getWeather() async {
-    Response response = await get('https://api.openweathermap.org/data/2.5/forecast?q=Tehran&units=metric&appid=121bfe664777c886e1481f7feb830455');
-    WeatherData weatherData  = weatherListFromJson(response.body);
-    Navigator.pushReplacementNamed(context, '/home', arguments: {
-      'email': widget.loginData['email'],
-      'password': widget.loginData['password'],
-      'weather': weatherData,
+    try {
+      Response response = await Dio().get('https://api.openweathermap.org/data/2.5/forecast?',
+          queryParameters: {
+            'q': 'Tehran',
+            'units': 'metric',
+            'appid': '121bfe664777c886e1481f7feb830455'
+          });
+      WeatherData weatherData = weatherListFromJson(response.toString());
+      Navigator.pushReplacementNamed(context, '/home', arguments: {
+        'email': widget.loginData['email'],
+        'password': widget.loginData['password'],
+        'weather': weatherData,
 
-    });
+      });
+    }catch(e){
+      print(e);
+    }
 
   }
 
